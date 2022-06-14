cd4e691c084ec189d750dc8876580e150af88ccf
marcelo.miotto <mmiotto@gmail.com>
2022-06-14 15:58:39 -0300

    feat: complete the code for all methods.

--- dev_flask_restful/app.py
+++ dev_flask_restful/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,22 +9,39 @@ altcoins = {"BabyDoge": "Baby Doge Coin",
             "RACA": "RadioCaca"}
 
 
+def abort_if_token_doesnt_exist(token):
+    if token not in altcoins:
+        abort(404, message=f"Token {token} doesn't exist")
+
+
 class Altcoin(Resource):
 
-    def get(self, token_id: int = None):
-        return altcoins
+    def get(self, token: str = None):
+        if token is None:
+            return altcoins
+        else:
+            abort_if_token_doesnt_exist(token=token)
+            return jsonify({token: altcoins[token]})
 
     def post(self):
-        return altcoins
+        tokens = request.get_json()
+        altcoins.update(tokens)
+        print(altcoins)
+        return altcoins, 201
 
-    def put(self, token_id: int):
-        return altcoins
+    def put(self, token: str):
+        abort_if_token_doesnt_exist(token=token)
+        tokens = request.get_json()
+        altcoins[token] = tokens[token]
+        return altcoins, 201
 
-    def delete(self, token_id: int):
-        return altcoins
+    def delete(self, token: str):
+        abort_if_token_doesnt_exist(token=token)
+        del altcoins[token]
+        return "", 204
 
 
-api.add_resource(Altcoin, "/", "/<token_id>")
+api.add_resource(Altcoin, "/", "/<token>")
 
 
 if __name__ == '__main__':
